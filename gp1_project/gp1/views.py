175f88ceab9bdd2bf22ad142ca0b446fec18f8cf
Cemal <cemalkureli@gmail.com>
2022-12-08 22:26:38 +0300

    GET isteğinde veya text area'ya veri girilmediğinde default değer problemi ortadan kalktı

--- gp1_project/gp1/views.py
+++ gp1_project/gp1/views.py
@@ -20,26 +20,23 @@ def appointment(request):
 
 def complaints(request):
     import pickle
-    empty_str = ""
     mymodel = pickle.load(open("templates/pickle_folder/hasta_yorum.pickle","rb"))
     sentence = (str(request.POST.get("complaint"))).lower()
-    print("asdasd",sentence)
-    if sentence != empty_str:
-        kategori = mymodel.predict([sentence])
-        print(kategori)
-        dictionary = {0 : 'Kulak Burun Boğaz (KBB)', 1 :'Enfeksiyon Hastalıkları', 2 :'Beslenme ve Diyetetik Diyetisyen',
-                    3 :'Göz Hastalıkları', 4 : 'Onkoloji', 5 :'Kadın Hastalıkları ve Doğum', 6 :'Psikiyatri', 
-                    7 :'Ortopedi ve Travmatoloji', 8 :'Kardiyoloji', 9 :'Beyin ve Sinir Cerrahisi',10 :'Fiziksel Tıp ve Rehabikseilitasyon', 
-                    11 :'Göğüs Hastalıkları', 12 :'Dermatoloji', 13 :'İç Hastalıkları',14 :'Çocuk Hastalığı ve Hastalıkları', 15 :'Diş Hekimliği', 
-                    16 :'Üroloji'}
-        category_name =dictionary.get(*kategori)
-        context = {
-            "machine_translate" : category_name
-            }
-        print(category_name)
-    else:
-        context = {
-            "machine_translate_empty" : empty_str
-            }
 
-    return render(request,"complaints.html",context)
+    kategori = mymodel.predict([sentence])
+    dictionary = {0 : 'Kulak Burun Boğaz (KBB)', 1 :'Enfeksiyon Hastalıkları', 2 :'Beslenme ve Diyetetik Diyetisyen',
+                3 :'Göz Hastalıkları', 4 : 'Onkoloji', 5 :'Kadın Hastalıkları ve Doğum', 6 :'Psikiyatri', 
+                7 :'Ortopedi ve Travmatoloji', 8 :'Kardiyoloji', 9 :'Beyin ve Sinir Cerrahisi',10 :'Fiziksel Tıp ve Rehabilitasyon', 
+                11 :'Göğüs Hastalıkları', 12 :'Dermatoloji', 13 :'İç Hastalıkları',14 :'Çocuk Hastalığı ve Hastalıkları', 15 :'Diş Hekimliği', 
+                16 :'Üroloji'}
+    category_name =dictionary.get(*kategori)
+    context = {
+        "machine_translate" : category_name,
+        }
+    if request.method == "GET" or sentence == "":
+        return render(request,"complaints.html",{})
+    else:
+        return render(request,"complaints.html",context)
+   
+    
+    
