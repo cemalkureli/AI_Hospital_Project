79e63917c285b7158cff8ab76653d6a7d1ad02b7
Cemal Kureli <91951427+cemalkureli@users.noreply.github.com>
2024-11-06 08:26:48 +0300

    Update settings.py

--- gp1_project/gp1_project/settings.py
+++ gp1_project/gp1_project/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h()vzj&q(49421cjh+x)_76_ssw4ot54zjn^om7rj2trj+*e^)'
+SECRET_KEY = '-------------------------------------------------------------'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -152,6 +152,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.office365.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'gp_project_deneme@hotmail.com'
-EMAIL_HOST_PASSWORD = 'lkgunhilgxwpawov'
+EMAIL_HOST_USER = '----------------------------'
+EMAIL_HOST_PASSWORD = '------------------------'
 EMAIL_ENCRYPTION='STARTTLS'
